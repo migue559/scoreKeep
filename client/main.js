7caed154896b5c541d2442476c5e20c1a9f1b1dc
migue559 <miguelangel.cruz@kumma.com.mx>
2017-05-29 11:18:16 -0500

    remove update added

--- client/main.js
+++ client/main.js
@@ -5,28 +5,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+import {Players} from './../imports/api/players';
 
-const players=[{
-	_id:'1',  name:'Edgar' ,score:99},{
-	_id:'2',  name:'Javier',score:99},{
-	_id:'3',  name:'Luis'  ,score:98},{
-	_id:'4',  name:'Miguel',score:9}]
 
-const render_players= function(playersList){
+
+//const players=[{
+//	_id:'1',  name:'Edgar' ,score:99},{
+//	_id:'2',  name:'Javier',score:99},{
+//	_id:'3',  name:'Luis'  ,score:98},{
+//	_id:'4',  name:'Miguel',score:9}]
+
+//const render_players= function (playersList)  {
+const render_players= (playersList) => {
 	//let numbers=[{val:1},{val:2},{val:3},{val:123}];	
-	return   playersList.map(function(player){
-		return <p key ={player._id}>{ player.name} has {player.score} point(s).</p>;
+	//return   playersList.map(function(player)  {
+	return   playersList.map((player) => {
+		return (
+			<p key ={player._id}>
+			{ player.name} has {player.score} point(s).
+			<button onClick={()=>{
+				//alert('deleted')
+				Players.update({_id:player._id},{$inc:{score:1}})
+			}}> +1 </button>
+			<button onClick={()=>{
+				//alert('deleted')
+				Players.update({_id:player._id},{$inc:{score:-1}})
+			}}> -1 </button>
+			<button onClick={()=>{
+				//alert('deleted')
+				Players.remove(player._id)
+			}}> X </button>
+			</p>
+			);
 	});
 };
 
-  Meteor.startup(    function	(){let title = 'Score Keep';
-	let name = 'Maicol';
-	let jsx=(<div>
+
+//const handleSubmit = function( event )   {
+const handleSubmit = ( event ) =>  {
+	let playerName_=event.target.playerName_.value;  //target se refiere al form de html
+	event.preventDefault();
+	if (playerName_){
+		event.target.playerName_.value='';
+		Players.insert({ 	name:playerName_  ,		score:0	});
+					}
+				};
+
+//Meteor.startup( function ()  {
+Meteor.startup(    	() => {
+	// call tracker autorun
+	// create variable  call players -> set equals  to fetch query
+	// render  players to the screen
+	// Tracker.autorun( function()  {
+	Tracker.autorun(() => {
+		let players=Players.find().fetch();
+		let title = 'Score Keep';
+		let name = 'Maicol';
+		let jsx=(
+			<div>
 				<h1>{ title }</h1>
 				<p> Una cadena pitera de { name } ¡</p> 
 				<p> segunda cadena pitera</p>
 				{ render_players(players) }
+				<form onSubmit={handleSubmit}>
+					<input type='text' name='playerName_' placeholder='Player Name'></input> 
+					<botton>Add Player</botton>
+				</form>
 			</div>
 			);
 	ReactDOM.render(jsx, document.getElementById('app'));
+	});
 });
+//colore
--- server/main.js
+++ server/main.js
@@ -1,9 +1,46 @@
-import {greetUser} from './../imports/utils';
-import {add} from './../imports/math';
-import rest from './../imports/math';
+import {Meteor}  from 'meteor/meteor';
+import {Players} from './../imports/api/players';
 
-console.log('Log from /server/main.js')
 
-console.log(greetUser())
-console.log(add(5,4))
-console.log(rest(5,4))
+Meteor.startup( function() {
+//	Players.insert({ 	name:'mike',		score:5	});
+//	Players.update({    name:'mike'                 }, { $set: { score: 45 } } );
+	//console.log(Players.find().fetch());
+
+
+let squere_1 = function(x){                   //ways to create functios
+	return x*x;
+};
+let squere_2 = (x) => {                       //ways to create functios
+	return x*x;
+};
+
+let squere_3 = (x) => (x*(x+1));              //ways to create arrow functios
+
+let user ={
+	name:'Maicol',
+	sayHi: function(){
+		console.log("Hola"+" "+this.name)           // this es uado para tomar valores de esta instancia
+	},
+	sayHi2: ()=>{
+		console.log("Hola"+" "+this.name)           // this  es uado NNNOOOOOOO tomar valores de esta instancia
+	},
+	sayHi3: ()=>{
+		console.log(arguments)           // this  es uado NNNOOOOOOO tomar valores de esta instancia
+	},
+	sayHi4: function () {
+		console.log(arguments)           // this  es uado NNNOOOOOOO tomar valores de esta instancia
+	}	
+}
+
+console.log(squere_1(9));
+console.log(squere_2(9));
+console.log(squere_3(9));
+
+user.sayHi();
+user.sayHi2();
+user.sayHi3(45,53);
+user.sayHi4(45,53);
+
+
+});
